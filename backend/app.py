71c65444e1d982bcf008febf4da3298782b3ae44
Khanh Nguyen <khanhng3009@gmail.com>
2025-04-01 20:23:48 +0700

    Finish

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ def index():
             host="db",
             user="root",
             password="123456",
-            database="labdb"
+            database="labdb",
         )
         cursor = conn.cursor()
         cursor.execute("SELECT 'Hello from MySQL via Flask API'")
@@ -20,4 +20,4 @@ def index():
         return jsonify(error=str(e))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
